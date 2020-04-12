a544be631dfe9a5469ae9d26026d25691d9af9f1
thanhn <510thanh.ngo@gmail.com>
2020-04-12 02:40:09 -0700

    Modify to get first and last name of users at register

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,33 +3,35 @@ $(document).ready(function() {
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
-  var firstNameInput = $("input#firstname-input");
-  var lastNameInput = $("input#lastname-input");
+  var firstNameInput = $("input#firstName-input");
+  var lastNameInput = $("input#lastName-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function(event) {
+
     event.preventDefault();
     var userData = {
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim(),
-      firstName: lastNameInput.val().trim(),
-      lastName: firstNameInput.val().trim()
+      email: emailInput.val(),
+      password: passwordInput.val(),
+      firstName: lastNameInput.val(),
+      lastName: firstNameInput.val(),
     };
-
-    if (!userData.email || !userData.password || userData.firstName || userData.lastName) {
+    if (!userData.email || !userData.password || !userData.firstName || !userData.lastName) {
       return;
     }
     // If we have an email and password, run the signUpUser function
+
     signUpUser(userData.email, userData.password, userData.firstName, userData.lastName);
     emailInput.val("");
     passwordInput.val("");
     firstNameInput.val("");
-    lastNameINput.val("");
+    lastNameInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(email, password, firstName, lastName) {
+
     $.post("/api/signup", {
       email: email,
       password: password,
